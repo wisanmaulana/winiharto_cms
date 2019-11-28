104a5da73532cbc6bc17227b38a4f532d49e05fb
wisanmaulana <wisan.maulana@gmail.com>
2019-11-28 18:13:33 +0700

    update winiharto depan

--- application/controllers/Customers.php
+++ application/controllers/Customers.php
@@ -1,18 +1,9 @@
 <?php if(!defined("BASEPATH")) exit("Hack Attempt");
 class Customers extends CI_Controller {
 
-    public function __construct() {
-      parent::__construct();
-      $this->load->model('M_nav', 'nav');
-      // $this->output->enable_profiler(TRUE);
-          // Your own constructor code
-    }
-
     public function index(){
 
-      $data['navParent'] = $this->nav->getNavigationParent();
-
-      $this->load->view('templates/header', $data);
+      $this->load->view('templates/header');
       $this->load->view('pages/customers');
       $this->load->view('templates/footer');
     }
--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -6,48 +6,79 @@ class Product extends CI_Controller {
       public function __construct()
       {
               parent::__construct();
-              // $this->output->enable_profiler(TRUE);
-              $this->load->model('M_products', 'products');
-              $this->load->model('M_nav', 'nav');
+              $this->load->model('Model_product', 'model_product');
       }
 
       //Searching Products
       public function search() {
 
-        $query                  = $this->input->get('search');
-        $data['navParent']      = $this->nav->getNavigationParent();
-        $data['query_product']  = $this->products->searchProducts($query);
-        $data['keyword']        = $query;
-
-        $this->load->view('templates/header', $data);
-        $this->load->view('pages/products/search_results', $data);
+        $this->load->view('templates/header');
+        $this->load->view('pages/products/industrial/search_results', $data);
         $this->load->view('templates/footer');
 
       }
 
-      public function brand() {
+      /* NEW BRAND TO SHOW PRODUCTS */
+      public function brand_category() {
 
-        $brandName = $this->input->get('name');
+        $getDetails = $this->uri->segment(3);
 
-        $data['navParent']      = $this->nav->getNavigationParent();
-        $data['query_product']  = $this->products->selectProducts($brandName);
-        $data['query_title']    = $this->products->selectBrandName($brandName);
+        $data['query_product'] = $this->model_product->select_product($getDetails);
+        $data['query_title'] = $this->model_product->search_product_title_subcategory($getDetails);
 
-        $this->load->view('templates/header', $data);
+        $this->load->view('templates/header');
+        $this->load->view('pages/products/product_result', $data);
+        $this->load->view('templates/footer');
+      }
+
+      /* NEW BRAND TO SHOW PRODUCTS */
+      public function product_category() {
+        //$this->output->enable_profiler(TRUE);
+
+        $getDetails = $this->uri->segment(2);
+
+        $data['query_product'] = $this->model_product->select_our_products($getDetails);
+        $data['query_title'] = $this->model_product->select_product_title($getDetails);
+
+        $this->load->view('templates/header');
+        $this->load->view('pages/products/product_result', $data);
+        $this->load->view('templates/footer');
+      }
+
+      public function brand_subcategory() {
+
+        $getDetails = $this->uri->segment(2);
+
+        $data['query_product'] = $this->model_product->select_category($getDetails);
+        $data['query_title'] = $this->model_product->search_product_title($getDetails);
+
+        $this->load->view('templates/header');
         $this->load->view('pages/products/product_result', $data);
         $this->load->view('templates/footer');
 
       }
 
-      public function products() {
+      public function show_products() {
 
-        $productName = $this->input->get('name');
+        $getDetails = $this->uri->segment(2);
 
-        $data['navParent']      = $this->nav->getNavigationParent();
-        $data['query_product']  = $this->products->selectProducts($productName);
-        $data['query_title']    = $this->products->selectProductName($productName);
+        $data['query_product'] = $this->model_product->select_product($getDetails);
+        $data['query_title'] = $this->model_product->search_product_title($getDetails);
 
-        $this->load->view('templates/header', $data);
+        $this->load->view('templates/header');
+        $this->load->view('pages/products/product_result', $data);
+        $this->load->view('templates/footer');
+
+      }
+
+      public function show_products_subcategory() {
+
+        $getDetails = $this->uri->segment(2);
+
+        $data['query_product'] = $this->model_product->select_product_by_subcategory($getDetails);
+        $data['query_title'] = $this->model_product->search_product_title($getDetails);
+
+        $this->load->view('templates/header');
         $this->load->view('pages/products/product_result', $data);
         $this->load->view('templates/footer');
 
